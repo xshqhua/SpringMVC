b6e77446a04d1945768f9b5d666a74878e0758e5
xushihua <xshqhua@gmail.com>
2017-07-09 23:00:41 +0800

    add *.xml file content

---
+++ src/com/zte/test/Test01.java
@@ -0,0 +1,11 @@
+package com.zte.test;
+/**
+ * package:com.zte.test
+ * file:Test01.java
+ * author:XSH
+ * email:xshqhua@gmail.com
+ * datetime:2017年7月9日 下午10:55:48
+ */
+public class Test01 {
+	
+}
